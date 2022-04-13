ffbc38efb9a8205526b7b5c89763901f51ce4e29
EdersonSouza <ederson.souza.mg@gmail.com>
2022-04-13 15:20:43 -0400

    estilizando

--- src/components/DivoltisPorris.vue
+++ src/components/DivoltisPorris.vue
@@ -3,6 +3,7 @@
   <v-div class="block galleryBlock">
     <v-container>
 
+
       <v-card
   
             
@@ -10,107 +11,123 @@
         <h2 class="text-left">top cacilds</h2>
         <v-col></v-col>
         <v-container>
-
-            <v-row >
-                <v-col  class="d-flex child-flex mx-auto " cols="12"
-                md="7"
-                offset-md="5"  height="100%" max-width="374"
-                >
-                    <v-card >
-                        <v-img
-                            src="../assets/divoltis/mussum.png"
-                        ></v-img>
-
-                        <v-card-title class="primary--text">Consentis</v-card-title>
-                        <v-card-text class="black--text">
-                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
-                                <v-spacer></v-spacer>
-                                <v-p> Sapien in monti palavri</v-p>
-                                        
-                        </v-card-text>
-
-                    </v-card>
-                    <v-spacer></v-spacer>
-
-                    <v-card >
-                        <v-img
-                            src="../assets/divoltis/divoltis_porris-02.png"
-                        ></v-img>
-
-                        <v-card-title class="primary--text">Consentis</v-card-title>
-                        <v-card-text class="black--text">
-                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
-                                <v-spacer></v-spacer>
-                                <v-p> Sapien in monti palavri</v-p>
-                                        
-                        </v-card-text>
-
-                    </v-card>
-                    
-                </v-col>
-                <v-col  class="d-flex child-flex mx-auto " cols="12"
-                md="7"
-                offset-md="5" height="100%" max-width="374"
-                >
-                    <v-card >
-                        <v-img
-                            src="../assets/divoltis/divoltis_porris-03.png"
-                        ></v-img>
-
-                        <v-card-title class="primary--text">Consentis</v-card-title>
-                        <v-card-text class="black--text">
-                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
-                                <v-spacer></v-spacer>
-                                <v-p> Sapien in monti palavri</v-p>
-                                        
-                        </v-card-text>
-
-                    </v-card>
-                    <v-spacer></v-spacer>
-
-                    <v-card >
-                        <v-img
-                            src="../assets/divoltis/divoltis_porris-04.png"
-                        ></v-img>
-
-                        <v-card-title class="primary--text">Consentis</v-card-title>
-                        <v-card-text class="black--text">
-                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
-                                <v-spacer></v-spacer>
-                                <v-p> Sapien in monti palavri</v-p>
-                                        
-                        </v-card-text>
-
-                    </v-card>
-                    
-                </v-col>
+            
+            <v-row warap>
                 
+                    <v-col>
+                        <v-layout row warap>
+                            
+                                <v-col  class="d-flex child-flex mx-auto my-12 " 
+                                
+                                >
+                                    <v-card >
+                                        <v-img
+                                            src="../assets/divoltis/mussum.png"
+                                        ></v-img>
 
+                                        <v-card-title class="primary--text">Consentis</v-card-title>
+                                        <v-card-text class="black--text">
+                                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
+                                                <v-spacer></v-spacer>
+                                                <v-p> Sapien in monti palavri</v-p>
+                                                        
+                                        </v-card-text>
+
+                                    </v-card>
+                                </v-col>
+                                    
+                                <v-col  class="d-flex child-flex mx-auto my-12 " 
+                                
+                                >
+                                    <v-card >
+                                        <v-img
+                                            src="../assets/divoltis/divoltis_porris-02.png"
+                                        ></v-img>
+
+                                        <v-card-title class="primary--text">Consentis</v-card-title>
+                                        <v-card-text class="black--text">
+                                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
+                                                <v-spacer></v-spacer>
+                                                <v-p> Sapien in monti palavri</v-p>
+                                                        
+                                        </v-card-text>
+
+                                    </v-card>
+                                    
+                                </v-col>
+                                <v-col  class="d-flex child-flex mx-auto my-12 " 
+                                >
+                                    <v-card >
+                                        <v-img
+                                            src="../assets/divoltis/divoltis_porris-03.png"
+                                        ></v-img>
+
+                                        <v-card-title class="primary--text">Consentis</v-card-title>
+                                        <v-card-text class="black--text">
+                                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
+                                                <v-spacer></v-spacer>
+                                                <v-p> Sapien in monti palavri</v-p>
+                                                        
+                                        </v-card-text>
+
+                                    </v-card>
+                                </v-col>    
+                                <v-col  class="d-flex child-flex mx-auto my-12 " 
+                                
+                                >
+                                    <v-card >
+                                        <v-img
+                                            src="../assets/divoltis/divoltis_porris-04.png"
+                                        ></v-img>
+
+                                        <v-card-title class="primary--text">Consentis</v-card-title>
+                                        <v-card-text class="black--text">
+                                                <v-p>Manduma pindureta quium dia nois paga</v-p> 
+                                                <v-spacer></v-spacer>
+                                                <v-p> Sapien in monti palavri</v-p>
+                                                        
+                                        </v-card-text>
+
+                                    </v-card>
+                                    
+                                </v-col>
+
+                           
+                    
+                        
+
+                        </v-layout>
+
+                    </v-col>
+                    <v-col>
+                        <v-row>
+                            <v-flex md6>
+                                <v-col>
+                                    <v-card>
+                                        <v-card-text align="right">
+                                            <v-cartd-title >publicidade</v-cartd-title>
+                                        </v-card-text>
+                                        <v-img
+                                            src="../assets/divoltis/propaganda.png">
+                                        </v-img>
+                                    </v-card>
+                                </v-col>
+                            </v-flex>
+                           
+                        </v-row>
+
+                    </v-col>
+                
             </v-row>
+
+            
 
         </v-container>
         
-        
-            
-            
             
             
         </v-card>
-        <v-container>
-            <v-row>
-                    <v-card >
-                        <v-img
-                            src="../assets/divoltis/propaganda.png"
-                        ></v-img>
-
-
-                    </v-card>
-
-              
-
-            </v-row>
-
-        </v-container>
+        
      
     </v-container>
     
